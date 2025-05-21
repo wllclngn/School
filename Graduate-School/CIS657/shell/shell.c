4029deb95f8a8bb36ffa8b00ca44d8bde175cf8e
Will C. <wu352@hotmail.com>
2025-05-20 21:25:33 -0500

    Updates; CIS657.

--- Graduate-School/CIS657/shell/shell.c
+++ Graduate-School/CIS657/shell/shell.c
@@ -1,8 +1,7 @@
-// Add these entries to cmdtab[]
+// Add the following entries to cmdtab[]
 {"create",  xsh_create},
 {"createsleep", xsh_createsleep},
 {"psready", xsh_psready},
 {"wait", xsh_wait},
 {"signaln", xsh_signaln},
 {"resumen", xsh_resumen},
-// ...rest of table...
