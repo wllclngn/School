ff0335af98518b70852b76ca5ad8a79f50fe4d51
Will C. <wuz352@hotmail.com>
2025-06-15 10:30:56 -0700

    Updates.

--- Graduate-School/CIS657/FINAL/system/resched.c
+++ Graduate-School/CIS657/FINAL/system/resched.c
@@ -1,26 +1,23 @@
-/* resched.c - Modified for XINU Final Project
- * Last modified: 2025-06-15 06:42:05 UTC
+/* resched.c - Final Version for Tonight
+ * Last modified: 2025-06-15 07:19:51 UTC
  * Modified by: wllclngn
+ *
+ * LAST ATTEMPT: If this doesn't work, we'll tackle it 
+ * after some much-needed rest.
  */
 
 #include <xinu.h>
 #include <pstarv.h>
 
+// Global round-robin counter
+static unsigned int rr_count = 0;
+
 void boost_pstarv_priority(void) {
     if (enable_starvation_fix && pstarv_pid != BADPID) {
         struct procent *pstarv = &proctab[pstarv_pid];
-        
-        if (pstarv->prstate == PR_READY && pstarv_pid != currpid) {
-            int old_prio = pstarv->prprio;
-            pstarv->prprio = (pstarv->prprio + 2 > 50) ? 50 : pstarv->prprio + 2;
-            
-            kprintf("\nBOOST: PStarv priority increased from %d to %d\n", 
-                    old_prio, pstarv->prprio);
-            
-            // Force preemption after boost if PStarv's new priority is higher
-            if (pstarv->prprio > proctab[currpid].prprio) {
-                resched();
-            }
+        if (pstarv->prstate == PR_READY) {
+            pstarv->prprio = 50;  // Maximum priority
+            kprintf("\nBOOST: PStarv set to MAXIMUM priority 50\n");
         }
     }
 }
@@ -36,11 +33,20 @@ void resched(void) {
 
     ptold = &proctab[currpid];
 
-    // Force rescheduling after each quantum
+    /* Absolute simplest approach possible:
+     * 1. ALWAYS put current process back in ready queue
+     * 2. ALWAYS pick next process from ready queue
+     * 3. Every 3rd switch, boost PStarv to max priority
+     */
     if (ptold->prstate == PR_CURR) {
         ptold->prstate = PR_READY;
         insert(currpid, readylist, ptold->prprio);
-        boost_pstarv_priority();  // Check for boost before selecting next process
+
+        rr_count++;
+        if (rr_count >= 3) {
+            boost_pstarv_priority();
+            rr_count = 0;
+        }
     }
 
     currpid = dequeue(readylist);
